f6830e4f4d424714bce4abf7251ab3e02daf75c9
Dalu-codes <chidaluezealigo@gmail.com>
2026-07-06 19:24:05 +0100

    Config updated

--- src/main/java/com/project/task_manager/security/SecurityFilterConfig.java
+++ src/main/java/com/project/task_manager/security/SecurityFilterConfig.java
@@ -1,12 +1,9 @@
 package com.project.task_manager.security;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,27 +14,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityFilterConfig {
+
     private final AuthFilter authFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, CorsConfigurationSource corsConfigurationSource) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests
-               (req -> req.requestMatchers("/api/auth/**").permitAll()
-                       .anyRequest().authenticated())
+                        (req -> req.requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated())
                 .sessionManagement
-                (mag->mag.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        (mag -> mag.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
